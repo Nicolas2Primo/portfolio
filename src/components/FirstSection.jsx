5c4c2ee0e753af8ef0a5c6ed47b0e4e224178aff
Nicolas Primo <nicolasprimo@gmail.com>
2023-07-05 15:59:52 -0300

    animation test

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { IoDocumentText } from "react-icons/io5";
 import { PiHandWavingFill } from "react-icons/pi";
 
 const FirstSection = () => {
+  const [icons, setIcons] = useState([
+    {
+      icon: <AiFillLinkedin />,
+      link: "",
+    },
+    {
+      icon: <AiFillGithub />,
+      link: "",
+    },
+    {
+      icon: <IoDocumentText />,
+      link: "",
+    },
+  ]);
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="flex flex-col items-center md:items-start w-fit h-fit">
@@ -37,7 +51,7 @@ const FirstSection = () => {
           DEVELOPER
         </span>
         <div className="flex items-center justify-center w-full mt-2 text-center md:mt-3 md:justify-end md:text-start md:pr-12 h-fit">
-          <span className=" text-[10px] w-[250px] md:w-[410px] md:text-[12px] text-white font-titillium md:indent-12 leading-4 md:leading-5">
+          <span className=" text-[10px]  md:w-[410px] md:text-[12px] text-white font-titillium md:indent-12 leading-4 md:leading-5">
             TENHO 21 ANOS SOU DESENVOLVEDOR FRONTEND COM 1 ANO DE EXPERIÊNCIA
             TRABALHANDO COMO FREELANCER.
           </span>
@@ -46,17 +60,17 @@ const FirstSection = () => {
           <AiFillLinkedin
             size={28}
             color="#FEE6C2"
-            className="hover:translate-y-[-5px] hover:duration-300 hover:transition-transform focus:scale-125 "
+            className="hover:translate-y-[-5px] hover:duration-300 hover:transition-transform hover:scale-125 "
           />
           <AiFillGithub
             size={28}
             color="#FEE6C2"
-            className="hover:translate-y-[-5px] hover:duration-300 hover:transition-transform focus:scale-125 "
+            className="hover:translate-y-[-5px] hover:duration-300 hover:transition-transform hover:scale-125 "
           />
           <IoDocumentText
             size={28}
             color="#FEE6C2"
-            className="hover:translate-y-[-5px] hover:duration-300 hover:transition-transform focus:scale-125 "
+            className="hover:translate-y-[-5px] hover:duration-300 hover:transition-transform hover:scale-125 "
           />
         </div>
       </div>
