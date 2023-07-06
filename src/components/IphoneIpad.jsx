01ab57431761ebc797bb690dbf4cdac9651d5e40
Nicolas Primo <nicolasprimo@gmail.com>
2023-07-06 16:20:36 -0300

    teste

--- src/components/IphoneIpad.jsx
+++ src/components/IphoneIpad.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const IphoneIpad = ({ iphone, ipad }) => {
   return (
-    <div className="flex items-center justify-center w-full gap-2 h-fit">
-      <div className="flex items-end h-[415px]">
-        <img src={iphone} alt="" className="w-[150px]" />
+    <div className="flex items-center justify-center w-full h-full md:h-fit">
+      <div className="flex gap-2 cursor-pointer">
+        <div className="flex items-end h-[278px] md:h-[415px]">
+          <img src={iphone} alt="" className="w-[150px] md:w-[150px]" />
+        </div>
+        <img src={ipad} alt="" className="w-[200px] md:w-[300px]" />
       </div>
-      <img src={ipad} alt="" className="w-[300px]" />
     </div>
   );
 };
--- src/components/ProjectsSwiper.jsx
+++ src/components/ProjectsSwiper.jsx
@@ -5,6 +5,7 @@ import IphoneIpad from "./IphoneIpad";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const ProjectsSwiper = () => {
   const [projects, setProjects] = useState([
@@ -13,11 +14,11 @@ const ProjectsSwiper = () => {
       ipad: "./argosIpad.svg",
     },
     {
-      iphone: "./argosIphone.svg",
+      iphone: "./menmolistIphone.svg",
       ipad: "./argosIpad.svg",
     },
     {
-      iphone: "./argosIphone.svg",
+      iphone: "./netflixIphone.svg",
       ipad: "./argosIpad.svg",
     },
     {
@@ -25,13 +26,32 @@ const ProjectsSwiper = () => {
       ipad: "./argosIpad.svg",
     },
   ]);
+
+  const [breakpoints, setBreakpoints] = useState({
+    320: {
+      navigation: {
+        enabled: false,
+      },
+    },
+    768: {
+      navigation: {
+        enabled: true,
+      },
+    },
+  });
+
   return (
     <Swiper
+      breakpoints={breakpoints}
+      style={{
+        "--swiper-navigation-color": "#fff",
+        "--swiper-pagination-color": "#9D60CC",
+      }}
+      navigation
       pagination={true}
-      navigation={true}
       modules={[Pagination, Navigation]}
       slidesPerView={1}
-      className="w-full h-[470px]"
+      className="w-full h-[380px] md:h-[470px] cursor-grab"
     >
       {projects.map((project) => (
         <SwiperSlide className="">
