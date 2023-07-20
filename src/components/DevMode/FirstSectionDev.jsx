b0a4ca0d55c69621cdbeba46d1f9857864031b49
Nicolas Primo <nicolasprimo@gmail.com>
2023-07-20 11:46:28 -0300

    test

--- src/components/DevMode/FirstSectionDev.jsx
+++ src/components/DevMode/FirstSectionDev.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FirstSectionDev = () => {
   return (
     <div
-      className={`flex flex-col items-center justify-center w-full h-screen px-4 background-gradient-dev  overflow-x-hidden`}
+      className={`flex flex-col items-center justify-center w-full h-screen px-4 background-gradient-dev2 md:background-gradient-dev  overflow-x-hidden`}
     >
       <div className="absolute flex flex-col items-center justify-center right-4 top-4">
         <span className=" select-none font-bold text-[#917CAA] text-xs md:text-base font-titillium">
